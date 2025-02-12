3de0c6b63ba4cf047284cd4c7e1d066d7d58aef4
Your Name <you@example.com>
2025-02-12 16:16:28 +0600

    Last commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function main_page_1(){
+    window.location.href = "main_page_1.html";
+} 
+function main_page_2(){
+    window.location.href = "main_page_2.html";
+} 
+function main_page_3(){
+    window.location.href = "main_page_3.html";
+} 
+function main_page(){
+    window.location.href = "main_page.html";
+} 
+function shop(){
+    window.location.href = "shop.html";
+} 
+function shop_1(){
+    window.location.href = "shop_1.html";
+}
